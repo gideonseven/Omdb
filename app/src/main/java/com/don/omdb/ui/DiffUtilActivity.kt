cf3487a2159601cf0e9bdbe3aedee83c7870679e
Gideon <gideon@cicil.co.id>
2021-07-31 20:44:13 +0700

    update build

---
+++ app/src/main/java/com/don/omdb/ui/DiffUtilActivity.kt
@@ -0,0 +1,11 @@
+package com.don.omdb.ui
+
+
+/**
+ * Created by Gideon Steven Tobing on 31,July,2021.
+ * https://www.cicil.co.id/
+ * gideon@cicil.co.id
+ */
+class DiffUtilActivity: BaseActivity() {
+
+}
